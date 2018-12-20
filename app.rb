c92f4053b4def6d4148b30009be67b1d2efaa283
Makoto Ito <3mdevmail@gmail.com>
2018-12-21 00:08:28 +0900

    :bug: 多分動く

--- app.rb
+++ app.rb
@@ -18,12 +18,14 @@ tw_streaming = Twitter::Streaming::Client.new do |tw_config|
   tw_config.access_token_secret = config['access_token_secret']
 end
 
-topics = ['Music FMから楽曲', 'タップして聴きましょう。']
+topics = ['MusicFM', 'Music FM', 'MusicBox']
+puts "====="
 tw_streaming.filter(track: topics.join(',')) do |object|
+  puts "#{object.user.name}:\n#{object.text}"
+  puts object.user.id
+  puts "====="
   if object.text =~ /Music ?FMから(?:プレイリスト|楽曲)『.*』をシェアしました。タップして聴きましょう。/
-    tw_rest.block(object.id_str)
-    p "#{object.user.name}(#{object.user.screen_name}, #{object.user.id_str})をブロックしました"
-    p object.text
-    p "="
+    tw_rest.block(object.user.id)
+    p "\e#{object.user.name}(#{object.user.screen_name}, #{object.user.id})をブロックしました"
   end
 end
