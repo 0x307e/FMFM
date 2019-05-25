36dc9a43e817df4ead00484222dbf9bd4362cb70
Makoto Ito <3mdevmail@gmail.com>
2019-05-25 10:35:13 +0900

    :+1: 第3位「お気に入りプレイリスト」とかされても困る

--- app.rb
+++ app.rb
@@ -27,15 +27,19 @@ puts '====='
 tw_streaming.filter(track: topics.join(',')) do |object|
   user_status = redis.get object.user.id
   if object.text =~ /.*Music(?: (?:Box|FM)|Box|FM)から(?:プレイリスト|楽曲)『.*』をシェアしました。.*/
-    song_name = object.text[/『(.*)』/, 1]
-    res = Faraday.get(URI.encode("https://ws.audioscrobbler.com/2.0/?method=track.search&track=#{song_name}&api_key=#{config['last_fm']['api_key']}&format=json"))
-    if res.success?
-      song = JSON.parse res.body, {symbolize_names: true}
-      if song[:results][:trackmatches][:track].length != 0
-        artist_name = song[:results][:trackmatches][:track][0][:artist]
-        redis.zincrby 'rank/artist', 1, artist_name
+    if object.text.include('プレイリスト')
+      next
+    else
+      song_name = object.text[/『(.*)』/, 1]
+      res = Faraday.get(URI.encode("https://ws.audioscrobbler.com/2.0/?method=track.search&track=#{song_name}&api_key=#{config['last_fm']['api_key']}&format=json"))
+      if res.success?
+        song = JSON.parse res.body, {symbolize_names: true}
+        if song[:results][:trackmatches][:track].length != 0
+          artist_name = song[:results][:trackmatches][:track][0][:artist]
+          redis.zincrby 'rank/artist', 1, artist_name
+        end
+        redis.zincrby 'rank/song', 1, song_name
       end
-      redis.zincrby 'rank/song', 1, song_name
     end
     if user_status == nil
       tw_rest.block(object.user.id)
